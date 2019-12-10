d83dda481a4f46f7b6e2202f592c6f9e325a3da1
Ruben E Acosta <eldragondera1@gmail.com>
2019-12-10 16:59:01 -0500

    issue

--- src/controllers/issue.controller.ts
+++ src/controllers/issue.controller.ts
@@ -1,8 +1,46 @@
-// Uncomment these imports to begin using these cool features!
-
-// import {inject} from '@loopback/context';
+import {
+  repository,
+} from '@loopback/repository';
+import {
+  post,
+  getModelSchemaRef,
+  requestBody,
+} from '@loopback/rest';
+import { Issue } from '../models';
+import { IssueRepository } from '../repositories';
 
 
 export class IssueController {
-  constructor() {}
+  constructor(
+    @repository(IssueRepository)
+    public issueRepository: IssueRepository,
+  ) { }
+
+  @post('/issues', {
+    responses: {
+      '200': {
+        description: 'Issue creado',
+        content: { 'application/json': { schema: getModelSchemaRef(Issue) } },
+      },
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Issue, {
+            title: 'NewIssue',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    issue: Omit<Issue, 'id'>,
+  ): Promise<{}> {
+    await this.issueRepository.create(issue);
+    return {
+      statusCode: 200,
+      response: 'El issue fue creado correctamente'
+    }
+  }
 }
