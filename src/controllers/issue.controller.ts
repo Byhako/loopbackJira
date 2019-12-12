da6d4e61ffae09acc2b3024b7ae3f017a6c06859
Ruben E Acosta <eldragondera1@gmail.com>
2019-12-12 14:27:52 -0500

    validando post y put en issue

--- src/controllers/issue.controller.ts
+++ src/controllers/issue.controller.ts
@@ -13,13 +13,15 @@ import {
   requestBody,
 } from '@loopback/rest';
 import { Issue } from '../models';
-import { IssueRepository } from '../repositories';
+import { IssueRepository, ProyectoRepository } from '../repositories';
 
 
 export class IssueController {
   constructor(
     @repository(IssueRepository)
     public issueRepository: IssueRepository,
+    @repository(ProyectoRepository)
+    public proyectoRepository: ProyectoRepository,
   ) { }
 
   @post('/issues', {
@@ -43,10 +45,19 @@ export class IssueController {
     })
     issue: Omit<Issue, 'id'>,
   ): Promise<{}> {
-    await this.issueRepository.create(issue);
+    const existProyect = await this.proyectoRepository.findOne({
+      where: { id: issue.proyecto_id },
+    });
+    if (existProyect) {
+      await this.issueRepository.create(issue);
+      return {
+        statusCode: 200,
+        response: 'The issue was created correctly'
+      }
+    }
     return {
-      statusCode: 200,
-      response: 'El issue fue creado correctamente'
+      statusCode: 403,
+      response: 'The id proyect is incorect'
     }
   }
 
@@ -104,6 +115,43 @@ export class IssueController {
     return {
       statusCode: 403,
       response: 'The issue not exist',
+    }
+  }
+
+  @put('/issues/{id}', {
+    responses: {
+      '204': {
+        description: 'The issue is update',
+      },
+    },
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() issue: Issue,
+  ): Promise<{}> {
+    const exist = await this.issueRepository.findOne({
+      where: { id },
+    });
+
+    if (exist) {
+      const existProyect = await this.proyectoRepository.findOne({
+        where: { id: issue.proyecto_id },
+      });
+      if (existProyect) {
+        await this.issueRepository.replaceById(id, issue);
+        return {
+          statusCode: 200,
+          response: 'The user was edited correctly'
+        }
+      }
+      return {
+        statusCode: 403,
+        response: 'The id proyect is incorect',
+      }
+    }
+    return {
+      statusCode: 403,
+      response: 'This id of isssue is incorrect',
     }
   }
 }
