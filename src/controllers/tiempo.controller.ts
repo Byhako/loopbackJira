3c1e149e72254f324f2efda075931a39e7eaeb96
Ruben E Acosta <eldragondera1@gmail.com>
2019-12-12 15:34:19 -0500

    tiempos por usuario

--- src/controllers/tiempo.controller.ts
+++ src/controllers/tiempo.controller.ts
@@ -26,26 +26,6 @@ export class TiempoController {
     public proyectoRepository: ProyectoRepository,
   ) { }
 
-  @get('/tiempos', {
-    responses: {
-      '200': {
-        description: 'Array of Tiempo model instances',
-        content: {
-          'application/json': {
-            schema: {
-              type: 'array',
-              items: getModelSchemaRef(Tiempo, { includeRelations: true }),
-            },
-          },
-        },
-      },
-    },
-  })
-  async find(
-    @param.query.object('filter', getFilterSchemaFor(Tiempo)) filter?: Filter<Tiempo>,
-  ): Promise<Tiempo[]> {
-    return this.tiempoRepository.find(filter);
-  }
 
   @get('/tiempos/{usuario_id}/{fecha_inicio}/{fecha_fin}', {
     responses: {
@@ -68,108 +48,114 @@ export class TiempoController {
     @param.path.string('fecha_fin') fecha_fin: string,
     // @param.query.object('filter', getFilterSchemaFor(Tiempo)) filter?: Filter<Tiempo>,
   ): Promise<{}> {
-    const fechaInicio = moment.utc(fecha_inicio).format("YYYY:MM:DD");
-    const fechaFin = moment.utc(fecha_fin).format("YYYY:MM:DD");
-
-    // detalles de usuario
-    const usuario = await this.usuarioRepository.find({
+    const usuario = await this.usuarioRepository.findOne({
       where: { id: usuario_id },
     });
-    const nombre = usuario.map(item => item.nombre);
 
-    // tiempo por cada issue
-    const usuariosTimer = await this.tiempoRepository.find({
-      where: { usuario_id: usuario_id },
-    });
+    if (usuario) {
+      const fechaInicio = moment.utc(fecha_inicio);
+      const fechaFin = moment.utc(fecha_fin);
 
+      // time of every user
+      const users = await this.tiempoRepository.find({
+        where: {
+          and: [
+            { usuario_id },
+            { fecha: { gte: fechaInicio } },
+            { fecha: { lte: fechaFin } }
+          ]
+        },
+      });
 
-    const users = usuariosTimer.filter(item =>
-      moment.utc(item.fecha).format("YYYY:MM:DD") >= fechaInicio && moment.utc(item.fecha).format("YYYY:MM:DD") <= fechaFin
-    );
+      let tiempoTotal = moment('00:00:00', "HH:mm:ss");
+      const detalleLogs = users.map(item => {
+        const horaIni = moment(item.hora_inicio, "HH:mm:ss");
+        const horaFin = moment(item.hora_fin, "HH:mm:ss");
 
-    let tiempoTotal = moment('00:00:00', "HH:mm:ss");
-    const detalleLogs = users.map(item => {
-      const horaIni = moment(item.hora_inicio, "HH:mm:ss");
-      const horaFin = moment(item.hora_fin, "HH:mm:ss");
+        const diff = horaFin.diff(horaIni);
+        tiempoTotal = tiempoTotal + diff;
+        return {
+          horas_trabajadas: moment.utc(diff).format("HH:mm"),
+          issue_id: item.issue_id
+        }
+      })
+      tiempoTotal = moment(tiempoTotal).format("HH:mm");
 
-      const diff = horaFin.diff(horaIni);
-      tiempoTotal = tiempoTotal + diff;
-      return {
-        horasTabajadas: moment.utc(diff).format("HH:mm"),
-        issue_id: item.issue_id
-      }
-    })
-    tiempoTotal = moment(tiempoTotal).format("HH:mm");
+      // I group the times of each issue
+      const detalleLogsSum: any[] = [];
+      const issuesIds: number[] = [];
+      detalleLogs.forEach(item => {
+        const issue = item.issue_id;
+        const indexIssue = issuesIds.indexOf(issue);
+        if (indexIssue === -1) {
+          detalleLogsSum.push(item)
+          issuesIds.push(issue);
+        } else {
+          let indexDetail = 0;
+          let time = '';
+          detalleLogsSum.forEach((el, idx) => {
+            if (el.issue_id === issue) {
+              indexDetail = idx;
+              time = el.horas_trabajadas
+            }
+          })
+          time = moment(time, "HH:mm") + moment.utc(item.horas_trabajadas, "HH:mm")
+          time = moment(time).format("HH:mm");
+          detalleLogsSum.splice(indexDetail, 1)
+          detalleLogsSum.push({ horas_trabajadas: time, issue_id: issue })
+        }
+      })
 
-    const detalleLogsSum: any[] = [];
-    const issuesIds: number[] = [];
-    detalleLogs.forEach(item => {
-      const issue = item.issue_id;
-      const indexIssue = issuesIds.indexOf(issue);
-      if (indexIssue === -1) {
-        detalleLogsSum.push(item)
-        issuesIds.push(issue);
-      } else {
-        let indexDetail = 0;
-        let time = '';
-        detalleLogsSum.forEach((el, idx) => {
-          if (el.issue_id === issue) {
-            indexDetail = idx;
-            time = el.horasTabajadas
+      const issuesList = await this.issueRepository.find({
+        where: {
+          id: {
+            inq: issuesIds
+          },
+        },
+      });
+      const proyectosIds = issuesList.map(item => item.proyecto_id)
+
+      detalleLogsSum.forEach((item, idx) => {
+        const issueId = item.issue_id;
+        issuesList.forEach(is => {
+          if (issueId === is.id) {
+            detalleLogsSum[idx].proyecto_id = is.proyecto_id;
           }
         })
-        time = moment(time, "HH:mm") + moment.utc(item.horasTabajadas, "HH:mm")
-        time = moment(time).format("HH:mm");
-        detalleLogsSum.splice(indexDetail, 1)
-        detalleLogsSum.push({ horasTabajadas: time, issue_id: issue })
-      }
-    })
-
-    const issuesList = await this.issueRepository.find({
-      where: {
-        id: {
-          inq: issuesIds
-        },
-      },
-    });
-    const proyectosIds = issuesList.map(item => item.proyecto_id)
-
-    detalleLogsSum.forEach((item, idx) => {
-      const issueId = item.issue_id;
-      issuesList.forEach(is => {
-        if (issueId === is.id) {
-          detalleLogsSum[idx].proyecto_id = is.proyecto_id;
-        }
       })
-    })
 
-    const proyectosList = await this.proyectoRepository.find({
-      where: {
-        id: {
-          inq: proyectosIds
+      const proyectosList = await this.proyectoRepository.find({
+        where: {
+          id: {
+            inq: proyectosIds
+          },
         },
-      },
-    });
+      });
 
-    const proyectos = detalleLogsSum.map(item => {
-      const proyectId = item.proyecto_id;
-      let proyecItem = {};
-      proyectosList.forEach(pl => {
-        if (proyectId === pl.id) {
-          proyecItem = { horasTabajadas: item.horasTabajadas, nombreProyecto: pl.nombre };
-        }
+      const proyectos = detalleLogsSum.map(item => {
+        const proyectId = item.proyecto_id;
+        let proyecItem = {};
+        proyectosList.forEach(pl => {
+          if (proyectId === pl.id) {
+            proyecItem = { horas_trabajadas: item.horas_trabajadas, nombre_proyecto: pl.nombre };
+          }
+        })
+        return proyecItem;
       })
-      return proyecItem;
-    })
 
+      return {
+        statusCode: 200,
+        response: {
+          user: usuario.nombre,
+          tiempo_total: tiempoTotal,
+          proyectos,
+        }
+      };
+    }
     return {
-      statusCode: 200,
-      response: {
-        user: nombre[0],
-        tiempoTotal,
-        proyectos,
-      }
-    };
+      statusCode: 403,
+      response: 'The user not exist',
+    }
   }
 
   @get('/tiempos/{fecha_inicio}/{fecha_fin}', {
